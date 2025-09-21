2d3e76a4342f41ef1d2ebe591cca978e3180f71b
LadyK-AI <kelesetlogelo@gmail.com>
2025-09-21 05:00:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,13 +10,13 @@ import {
 } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA...yourKey...",
-  authDomain: "your-project.firebaseapp.com",
-  databaseURL: "https://your-project-default-rtdb.firebaseio.com",
-  projectId: "your-project",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdefg12345",
+  apiKey: "AIzaSyA8O6Fh10MNQrdW6cBZnUibVFRB2q5cD1Q",
+  authDomain: "if-i-were-c624c.firebaseapp.com",
+  databaseURL: "https://if-i-were-c624c-default-rtdb.firebaseio.com",
+  projectId: "if-i-were-c624c",
+  storageBucket: "if-i-were-c624c.firebasestorage.app",
+  messagingSenderId: "21442086633",
+  appId: "1:21442086633:web:e49f99337b3caaeaecd278",
 };
 
 let app, db;
