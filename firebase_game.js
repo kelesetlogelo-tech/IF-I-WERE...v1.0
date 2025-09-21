1d468e51ad542747a15803dba6ca38e8f8fdc35a
LadyK-AI <kelesetlogelo@gmail.com>
2025-09-21 03:11:06 +0200

    Update Firebase configuration in firebase_game.js

---
+++ firebase_game.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyANshEzcmmmxrfVcrOJz2SIUKzrqkwwB1A",
+  authDomain: "if-i-were-cabcf.firebaseapp.com",
+  databaseURL: "https://if-i-were-cabcf-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "if-i-were-cabcf",
+  storageBucket: "if-i-were-cabcf.firebasestorage.app",
+  messagingSenderId: "359013187632",
+  appId: "1:359013187632:web:f83a2b96173c6bd80e2df5"
+};
